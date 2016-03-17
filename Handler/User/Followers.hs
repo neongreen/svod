e3a4aa6d14ad123fc8380014e29df8d715f560b4
mrkkrp <markkarpov@opmbx.org>
2016-03-17 13:24:59 +0600

    Implement page displaying followers
    And update “follow user” widget accordingly.

--- Handler/User/Followers.hs
+++ Handler/User/Followers.hs
@@ -7,15 +7,41 @@
 -- Stability   :  experimental
 -- Portability :  portable
 --
--- Serve list of followers of a given user.
+-- Serve list of followers of particular user.
 
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE TemplateHaskell   #-}
 
 module Handler.User.Followers
   ( getUserFollowersR )
 where
 
+import Helper.Access (userViaSlug)
+import Helper.Json (userJson, paginatedJson)
 import Import
+import Widget.Pagination (lookupPagination, paginationW)
+import Widget.User (userW)
+import qualified Svod as S
 
-getUserFollowersR :: Slug -> Handler Html
-getUserFollowersR = undefined -- TODO provide better support in core
+-- | Serve list of followers of particular user.
+
+getUserFollowersR :: Slug -> Handler TypedContent
+getUserFollowersR slug = userViaSlug slug $ \user -> do
+  let uid = entityKey user
+      User {..} = entityVal user
+  params    <- lookupPagination
+  paginated <- runDB (S.followersPaginated params uid)
+  selectRep $ do
+    -- HTML representation
+    provideRep . defaultLayout $ do
+      setTitle (toHtml $ userName <> " (подписчики)")
+      $(widgetFile "user-followers")
+    -- JSON representation
+    provideRep $ do
+      render <- getUrlRender
+      items  <- forM (S.paginatedItems paginated) $ \user' -> do
+        let uid' = entityKey user'
+            val' = entityVal user'
+        followers <- runDB (S.followerCount uid')
+        return (userJson render followers val')
+      return (paginatedJson $ paginated { S.paginatedItems = items })
