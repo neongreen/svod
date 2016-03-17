4ff12a9e37b7ca957c2af3a9dcae6064cc62749a
mrkkrp <markkarpov@opmbx.org>
2016-03-17 15:19:23 +0600

    Add dumb version of release search page

--- Handler/SearchReleases.hs
+++ Handler/SearchReleases.hs
@@ -10,14 +10,37 @@
 -- General searching facility.
 
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE TemplateHaskell   #-}
 
 module Handler.SearchReleases
   ( getSearchReleasesR )
 where
 
+import Data.Maybe (fromJust)
+import Helper.Json (releaseJson, paginatedJson)
 import Import
+import Widget.Pagination (lookupPagination, paginationW)
+import Widget.Release (releaseW)
+import Widget.Search (searchW)
+import qualified Svod as S
 
 -- | General searching facility.
 
 getSearchReleasesR :: Handler TypedContent
-getSearchReleasesR = undefined -- TODO
+getSearchReleasesR = do
+  params    <- lookupPagination
+  paginated <- runDB (S.releaseQuery params [] []) -- FIXME
+  selectRep $ do
+    -- HTML representation
+    provideRep . noHeaderLayout $ do
+      setTitle "Поиск публикаций"
+      $(widgetFile "search-releases")
+    -- JSON representation
+    provideRep $ do
+      render <- getUrlRender
+      items  <- forM (S.paginatedItems paginated) $ \release -> do
+        let (Entity rid r@Release {..}) = release
+        stars <- runDB (S.starCount rid)
+        u     <- fromJust <$> runDB (get releaseArtist)
+        return (releaseJson render stars u r)
+      return (paginatedJson $ paginated { S.paginatedItems = items })
