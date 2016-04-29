6ea57fb9ab8821c898159297eb51c20a27773ed2
mrkkrp <markkarpov@opmbx.org>
2016-04-29 21:05:48 +0700

    Use ‘loadYamlSettings’ in ‘TestImport.hs’
    ‘loadAppSettings’ is deprecated.

--- test/TestImport.hs
+++ test/TestImport.hs
@@ -32,7 +32,7 @@ import Foundation            as X
 import Svod.Model            as X
 import Test.Hspec            as X
 import Text.Shakespeare.Text (st)
-import Yesod.Default.Config2 (ignoreEnv, loadAppSettings)
+import Yesod.Default.Config2 (ignoreEnv, loadYamlSettings)
 import Yesod.Test            as X
 
 runDB :: SqlPersistM a -> YesodExample App a
@@ -45,7 +45,7 @@ runDBWithApp app query = runSqlPersistMPool query (appConnPool app)
 
 withApp :: SpecWith (TestApp App) -> Spec
 withApp = before $ do
-  settings <- loadAppSettings
+  settings <- loadYamlSettings
     ["config/test-settings.yml", "config/settings.yml"]
     []
     ignoreEnv
